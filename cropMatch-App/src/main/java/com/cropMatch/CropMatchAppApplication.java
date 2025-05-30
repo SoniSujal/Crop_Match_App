e3b598d8e8a84b00677e22803856c52912252320
SujalSoni <sujalsoni1768@gmail.com>
2025-05-30 14:07:36 +0530

    Create Project Structure

---
+++ cropMatch-App/src/main/java/com/cropMatch/CropMatchAppApplication.java
@@ -0,0 +1,13 @@
+package com.cropMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropMatchAppApplication.class, args);
+	}
+
+}
